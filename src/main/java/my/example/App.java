64fd689395967280b2b81e2a15f046c683d63ae9
Zhamri Che Ani <zhamri@gmail.com>
2020-04-16 23:30:23 +0800

    Update MyClassTemplate

--- src/main/java/my/example/App.java
+++ src/main/java/my/example/App.java
@@ -1,8 +1,5 @@
 package my.example;
 
-/**
- * This is an example of Maven Structure
- */
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
